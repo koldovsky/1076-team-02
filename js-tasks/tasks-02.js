5926b9dd2ec62dc4866069199491d0a5894e8300
UlyanaKop <145978577+UlyanaKop@users.noreply.github.com>
2023-10-19 00:12:39 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -4,7 +4,9 @@ function stringToArray(string){
   }
 
 //DNA to RNA Conversion https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U");
+}
 //Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
 //Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
