46a61a793a05b54b6392a5a1bb846b467ae1529c
Irina Mishchenko <126945697+irinamishchenko@users.noreply.github.com>
2023-10-19 20:56:25 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -37,3 +37,14 @@ function min(arr, toReturn) {
     ? Math.min(...arr)
     : arr.indexOf(Math.min(...arr));
 }
+
+/////////
+
+function min(arr, toReturn) {
+  if (toReturn === "value") {
+    return Math.min(...arr);
+  } else {
+    return arr.indexOf(Math.min(...arr));
+  }
+}
+
