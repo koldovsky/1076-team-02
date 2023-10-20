eb97004c0deb43932317b2b783d4e965fe6e30a5
Maryna-511750 <109355692+Maryna-511750@users.noreply.github.com>
2023-10-20 13:18:59 +0000

    answer in task-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,7 +1,7 @@
 //Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
-  return string.split (" ");
-  }
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 //DNA to RNA Conversion https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
@@ -25,4 +25,31 @@ function min(arr, toReturn) {
     : arr.indexOf(Math.min(...arr));
 }
 
+//Поглиблені задачки (робити за бажанням):
+//A wolf in sheep's clothing https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 
+function warnTheSheep(queue) {
+  if (queue[queue.length - 1] === "wolf") {
+    return "Pls go away and stop eating my sheep";
+  }
+  return `Oi! Sheep number ${queue
+    .reverse()
+    .indexOf("wolf")}! You are about to be eaten by a wolf!`;
+}
+
+//Beginner - Lost Without a Map https://www.codewars.com/kata/beginner-lost-without-a-map
+
+function maps(x) {
+  return x.map((value) => value * 2);
+}
+
+//Find the first non-consecutive number https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+
+function firstNonConsecutive(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1] + 1) {
+      return arr[i];
+    }
+  }
+  return null;
+}
