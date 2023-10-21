71af79607f4bb323840e47c71fa4492e15a22844
Irina Mishchenko <126945697+irinamishchenko@users.noreply.github.com>
2023-10-21 14:14:04 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -26,3 +26,42 @@ function giveMeFive(obj){
   }
   return result
 }
+
+// Understanding closures - the basics
+
+// Fun with ES6 Classes #2 - Animals and Inheritance
+
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, status);
+    this.legs = 0;
+    this.species = "shark";
+    this.status = status;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, status);
+    this.legs = 4;
+    this.species = "cat";
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, status);
+    this.legs = 4;
+    this.species = "dog";
+    this.status = status;
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`
+  }
+}
