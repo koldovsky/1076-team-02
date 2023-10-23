9c363b150015a4d1863984b27c86283fb24059b2
Irina Mishchenko <126945697+irinamishchenko@users.noreply.github.com>
2023-10-23 16:56:27 +0300

    added formatted code

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -2,7 +2,7 @@
 
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    return Math.min(...args)
+    return Math.min(...args);
   }
 }
 
@@ -14,23 +14,22 @@ function circleCircumference(circle) {
 
 // Training JS #12: loop statement --for..in and for..of
 
-function giveMeFive(obj){
+function giveMeFive(obj) {
   const result = [];
-  for(let key in obj) {
-    if(key.length === 5) {
-      result.push(key)
+  for (let key in obj) {
+    if (key.length === 5) {
+      result.push(key);
     }
-    if(obj[key].length === 5) {
-      result.push(obj[key])
+    if (obj[key].length === 5) {
+      result.push(obj[key]);
     }
   }
-  return result
+  return result;
 }
 
 // Understanding closures - the basics
 
 // Fun with ES6 Classes #2 - Animals and Inheritance
-
 
 class Shark extends Animal {
   constructor(name, age, status) {
@@ -41,10 +40,9 @@ class Shark extends Animal {
 class Cat extends Animal {
   constructor(name, age, status) {
     super(name, age, 4, "cat", status);
-
   }
   introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
   }
 }
 
@@ -54,6 +52,6 @@ class Dog extends Animal {
     this.master = master;
   }
   greetMaster() {
-    return `Hello ${this.master}`
+    return `Hello ${this.master}`;
   }
 }
