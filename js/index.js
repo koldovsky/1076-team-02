043d9912ca27f543eb784fc2d08559f5d4b945bd
Maryna-511750 <109355692+Maryna-511750@users.noreply.github.com>
2023-10-29 21:01:56 +0000

    fixed section-carousel-price

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.addEventListener("partialsLoaded", () => {
-  import("./burger-menu.js");
-  import('./home-our-menu.js');
-  import('./blog.js');
-  import('./section-carousel-price.js');
+document.addEventListener("partialsLoaded", async () => {
+  await import("./burger-menu.js");
+  await import('./home-our-menu.js');
+  await import('./blog.js');
+  await import('./section-carousel-price.js');
 });
